204aae4b9c9b2aad25a782c9cee6b9cb9632fca3
TechLeads <anupoju.anand@gmail.com>
2020-07-15 21:38:47 +0530

    Added AngryProfessor problem

---
+++ AngryProfessor.java
@@ -0,0 +1,30 @@
+//package com.techleads.app;
+
+public class AngryProfessor {
+
+	public static void main(String[] args) {
+		int k=3;
+		//int k=2;
+		int[] a= {-1, -3, 4, 2};
+		//int[] a= {0, -1,0,0, 2, 1};
+		String angryProfessor = angryProfessor(k, a);
+		System.out.println(angryProfessor);
+
+	}
+	
+	static String angryProfessor(int k, int[] a) {
+		String result = "YES";
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] <= 0) {
+				count++;
+				if (k == (count)) {
+					result = "NO";
+					break;
+				}
+			}
+		}
+		return result;
+	}
+
+}
